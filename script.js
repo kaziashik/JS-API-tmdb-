4bb6bee1a780bccd395e76849002ab1a77490392
kaziashik <kazia096@gmail.com>
2021-09-15 20:14:35 +0800

    api pust

--- script.js
+++ script.js
@@ -11,8 +11,9 @@ const setPost = (posts) => {
         const postDiv = document.createElement('div')
         postDiv.classList.add("post")
         postDiv.innerHTML = `<div class="post p-5 bg-light border border-primary border-3 m-3">
-        <h3>${} </h3>
-        <p>description</p>
+        <h1>post no:${post.id}</h1>
+        <h3>${post.title} </h3>
+        <p>${post.body}</p>
     </div>`
         postContainer.appendChild(postDiv)
     }
