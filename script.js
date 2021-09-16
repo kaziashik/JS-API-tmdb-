9c131277f4f04008329dc87c343f2ba613f2241e
kaziashik <kazia096@gmail.com>
2021-09-16 12:09:45 +0800

    html push

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const setPost = (posts) => {
 
         const postDiv = document.createElement('div')
         postDiv.classList.add("post")
-        postDiv.innerHTML = `<div class="post p-5 bg-light border border-primary border-3 m-3">
+        postDiv.innerHTML = `<div class="post p-5 bg-light border border-primary border-3 m-3 ">
         <h1>post no:${post.id}</h1>
         <h3>${post.title} </h3>
         <p>${post.body}</p>
